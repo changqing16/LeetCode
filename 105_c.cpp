613fdd9b2085de34ac5ecd7c8b3eb317ca961371
changqing16 <ccqing996@gmail.com>
2019-04-27 23:59:38 +0800

    faster

--- 105_c.cpp
+++ 105_c.cpp
@@ -10,29 +10,63 @@ struct TreeNode
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+// TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
+// {
+//     if (preorder.empty())
+//         return nullptr;
+//     unordered_map<int, int> pos;
+//     for (int i = 0; i != inorder.size(); ++i)
+//         pos[inorder[i]] = i;
+//     TreeNode *root = new TreeNode(preorder[0]);
+//     stack<TreeNode *> todo;
+//     todo.push(root);
+//     for (int i = 1; i != preorder.size(); ++i)
+//     {
+//         TreeNode *node = new TreeNode(preorder[i]);
+//         if (pos[node->val] < pos[todo.top()->val])
+//             todo.top()->left = node;
+//         else
+//         {
+//             TreeNode *parent(nullptr);
+//             while (!todo.empty() && pos[node->val] > pos[todo.top()->val])
+//                 parent = todo.top(), todo.pop();
+//             parent->right = node;
+//         }
+//         todo.push(node);
+//     }
+//     return root;
+// }
 TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
 {
+    TreeNode *root = NULL;
+    stack<TreeNode *> MyData;
     if (preorder.empty())
-        return nullptr;
-    unordered_map<int, int> pos;
-    for (int i = 0; i != inorder.size(); ++i)
-        pos[inorder[i]] = i;
-    TreeNode *root = new TreeNode(preorder[0]);
-    stack<TreeNode *> todo;
-    todo.push(root);
-    for (int i = 1; i != preorder.size(); ++i)
+        return root;
+    root = new TreeNode(preorder[0]);
+    MyData.push(root);
+    int index = 0;
+    for (int i = 1; i <= preorder.size(); i++)
     {
-        TreeNode *node = new TreeNode(preorder[i]);
-        if (pos[node->val] < pos[todo.top()->val])
-            todo.top()->left = node;
+        TreeNode *cur = MyData.top();
+        if ((MyData.top()->val) != inorder[index])
+        {
+            cur->left = new TreeNode(preorder[i]);
+            MyData.push(cur->left);
+        }
         else
         {
-            TreeNode *parent(nullptr);
-            while (!todo.empty() && pos[node->val] > pos[todo.top()->val])
-                parent = todo.top(), todo.pop();
-            parent->right = node;
+            while (!MyData.empty() && ((MyData.top()->val) == inorder[index]))
+            {
+                cur = MyData.top();
+                MyData.pop();
+                index++;
+            }
+            if (index < inorder.size())
+            {
+                cur->right = new TreeNode(preorder[i]);
+                MyData.push(cur->right);
+            }
         }
-        todo.push(node);
     }
     return root;
 }
