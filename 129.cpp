d757bffcdeb2747957e856d7c23577d87b5fb0ea
changqing16 <ccqing996@gmail.com>
2019-06-19 15:52:37 +0800

    init

--- 129.cpp
+++ 129.cpp
@@ -10,6 +10,32 @@ struct TreeNode
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+int ans = 0;
+
+void dfs(TreeNode *root, int num)
+{
+    num = num * 10 + root->val;
+    if (root->left == NULL && root->right == NULL)
+    {
+        ans += num;
+        return;
+    }
+    if (root->left != NULL)
+        dfs(root->left, num);
+    if (root->right != NULL)
+        dfs(root->right, num);
+}
+
 int sumNumbers(TreeNode *root)
 {
+    dfs(root, 0);
+    return ans;
+}
+
+int main()
+{
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    cout << sumNumbers(root) << endl;
 }
